df1a09fe0913db0922c720173c8bafddededfc1a
Rafael Strack <strackinado@gmail.com>
2022-11-19 00:47:24 -0300

    melhoria em funções de verificação de tokens

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import ply.lex as lex
 import sys
+import ply.lex as lex
 
-palavrasReservadas = r'active|and|array|as|case|caseInsensitive|const|container|create|database|descending|disableDefault|doDefault|dynArray|else|enableDefault|endConst|endCreate|endFor|eEndForEach|endIf|endIndex|endMethod|endProc|endQuery|endRecord|endScan|endSort|endSwitch|endSwitchMenu|endTry|endType|endUses|endVar|endWhile|for|forEach|from|if|iif|in|index|indexStruct|is|key|lastMouseClicked|lastMouseRightClicked|like|loop|maintained|method|not|ObjectPAL|of|on|onFail|or|otherwise|passEvent|primary|proc|query|quitLoop|record|refIntStruct|retry|return|scan|secStruct|self|sort|step|struct|subject|switch|switchMenu|tag|then|to|try|type|unique|uses|var|where|while|with|without'
+palavrasReservadas = ['active', 'array', 'as', 'case', 'caseInsensitive', 'const', 'container', 'create', 'database', 'descending', 'disableDefault', 'doDefault', 'dynArray', 'else', 'enableDefault', 'endConst', 'endCreate', 'endFor', 'eEndForEach', 'endIf', 'endIndex', 'endMethod', 'endProc', 'endQuery', 'endRecord', 'endScan', 'endSort', 'endSwitch', 'endSwitchMenu', 'endTry', 'endType', 'endUses', 'endVar', 'endWhile', 'for', 'forEach', 'from', 'if', 'iif', 'in', 'index', 'indexStruct', 'is', 'key', 'lastMouseClicked', 'lastMouseRightClicked', 'like', 'loop', 'maintained', 'method', 'ObjectPAL', 'of', 'on', 'onFail', 'otherwise', 'passEvent', 'primary', 'proc', 'query', 'quitLoop', 'record', 'refIntStruct', 'retry', 'return', 'scan', 'secStruct', 'self', 'sort', 'step', 'struct', 'subject', 'switch', 'switchMenu', 'tag', 'then', 'to', 'try', 'type', 'unique', 'uses', 'var', 'where', 'while', 'with', 'without', 'SmallInt', 'LongInt', 'Number', 'String', 'Binary']
+regexPRS = '|'.join(palavrasReservadas)
+
+dictPRS = {pr:'PRS' for pr in palavrasReservadas}
 
 # List of token names.   This is always required
-tokens = (
+tokens = [
 'NUM',
 'LPAR',
 'RPAR',
@@ -20,7 +23,7 @@ tokens = (
 'PTO',
 'COM',
 'NIN'
-)
+]
 
 # Regular expression rules for simple tokens
 t_LPAR  = r'\('
@@ -34,17 +37,16 @@ def t_NUM(t):
     t.value = t.value 
     return t
 
-@lex.TOKEN(r'(and|or|Not|<(>|=)?|>(=)?|=)')
-def t_OARS(t):
-    t.value = t.value 
-    return t
-'''
-#@lex.TOKEN(r'(?!'+palavrasReservadas+ r')?')
-@lex.TOKEN(r'(?!for)?')
+@lex.TOKEN(r'[a-zA-Z_][a-zA-Z_0-9]*')
 def t_IDN(t):
-    t.value = t.value 
+    t.type = dictPRS.get(t.value,'IDN')
     return t
-'''
+
+@lex.TOKEN(r'(and|or|not|<(>|=)?|>(=)?|=)')
+def t_OARS(t):
+    t.value = t.value
+    return t
+
 @lex.TOKEN(r'=')
 def t_ATR(t):
     t.value = t.value 
@@ -52,39 +54,40 @@ def t_ATR(t):
 
 @lex.TOKEN(r'True|False')
 def t_BOO(t):
-    t.value = t.value 
+    t.type = dictPRS.get(t.value,'BOO')
     return t
 
 @lex.TOKEN(r'"[^"]*"')
 def t_STR(t):
     t.value = t.value 
     return t
+
 '''
 @lex.TOKEN(r'')
 def t_CAR(t):
     t.value = t.value 
     return t
 '''
-@lex.TOKEN(palavrasReservadas)
-def t_PRS(t):
-    t.value = t.value 
-    return t
+
 '''
 @lex.TOKEN(r'')
 def t_PTO(t):
     t.value = t.value 
     return t
 '''
+
 @lex.TOKEN(r';.*')
 def t_COM(t):
     t.value = t.value 
     return t
+
 '''
 @lex.TOKEN(r'')
 def t_NIN(t):
     t.value = t.value 
     return t
 '''
+
 # Define a rule so we can track line numbers
 def t_newline(t):
     r'\n+'
@@ -136,15 +139,3 @@ else:
     except IOError as err:
         if err.errno == 2:
             print(f"Erro: Arquivo não encontrado no diretório raiz.")
-
-''' VERSÃO WHILE TRUE
-# Give the lexer some input
-lexer.input(data)
-
-# Tokenize
-while True:
-    tok = lexer.token()
-    if not tok: 
-        break      # No more input
-    print(tok.lexpos)
-'''
